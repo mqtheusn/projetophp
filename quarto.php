2a7f73bba42da4e14f28dbea7ea99bc89ab1bdba
mqtheusn <matheuszor14@gmail.com>
2024-08-12 19:57:30 -0300

    quarto exercício

---
+++ quarto.php
@@ -0,0 +1,17 @@
+<?php
+
+	$data = "12 de agosto de 2024"
+	$salario = 850.00;
+	$idade = 18;
+	$cargo = 'estagiario';
+	$resultado = true;
+
+	echo "Data: $data <br />";
+	echo "Salario: $salario <br />";
+	echo "Idade: $idade <br />";
+	echo "Cargo: $cargo <br />";
+
+	if ($resultado) {
+		echo "Resultado é verdadeiro.";  
+	}
+?>
